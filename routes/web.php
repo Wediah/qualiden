08fe88f19d43aed38b7932900f6c633ed645408f
Emmanuel Wediah <100675006+Wediah@users.noreply.github.com>
2026-01-31 22:23:05 +0000

    ww

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,16 @@ Route::view('/quality', 'quality')->name('quality');
 Route::view('/health', 'health')->name('health');
 
 Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
 
 // Apply admin middleware to project management and dashboard
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::post('/store_projects', [ProjectController::class, 'store'])->name('projects.store');
     Route::view('/projects/create', 'projects.create')->name('projects.create');
+
+    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
 
     // Dashboard
     Route::view('dashboard', 'dashboard')
