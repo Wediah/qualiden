ac5272ed118d068babb6609996a7df7faf699b6e
Emmanuel Wediah <100675006+Wediah@users.noreply.github.com>
2026-01-31 02:17:56 +0000

    ww

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -169,7 +169,7 @@ mx-auto text-gray-900 dark:text-gray-100">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
                     </svg>
-                    {{ $project_id ? 'Update Project' : 'Create Project' }}
+                    Create Project
                 </span>
                 <span wire:loading wire:target="save, images" class="flex flex-row items-center">
                     <svg class="animate-spin h-3 w-3 mr-2 text-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
